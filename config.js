55add05ef76c7acd48a1e3883e22ae0ac9410daa
iqmath88 <iqmath88@gmail.com>
2026-08-17 02:09:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.MASAR_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
-  demoMode: true
+  supabaseUrl: 'https://uyzytmghqaidaeowdqi.w.supabase.co',
+  supabaseAnonKey: 'sb_publishable_N6YovBlaaoRaqgXlbBVHxg_NL6YpOcM',
+  demoMode: false
 };
