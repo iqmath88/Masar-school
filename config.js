9db6545e73d801aa7ba05ab72253bc76c9b4eddf
iqmath88 <iqmath88@gmail.com>
2026-08-17 02:24:54 +0300

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.MASAR_CONFIG = {
-  supabaseUrl: 'https://uyzytmghqaidaeowdqi.w.supabase.co',
+  supabaseUrl: 'https://uyzytmghqaidaeowdqiw.supabase.co',
   supabaseAnonKey: 'sb_publishable_N6YovBlaaoRaqgXlbBVHxg_NL6YpOcM',
   demoMode: false
 };
